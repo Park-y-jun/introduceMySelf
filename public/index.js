e94e29164c4e9e36099ed25b27d42c998c4c1393
ParkYoungJun <aptl741@gmail.com>
2023-01-06 01:00:29 +0900

    four

--- public/index.js
+++ public/index.js
@@ -64,10 +64,6 @@ const movie = () => {
   <button>X</button>`;
             movies.appendChild(videoDiv);
           });
-        //       `<iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="YouTube video player"
-        // frameborder="0"
-        // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        // allowfullscreen></iframe>`
       });
     }, 2000);
   });
